98af5d22dd622e7267ec5dc649f174df155f7326
NariDevLopes <maurolopes.dev@gmail.com>
2024-01-02 11:36:34 -0300

    feat: Created a website to generate passwords

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+// Seleção de elementos 
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+
+// Funções 
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+};
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+};
+
+const getNumber = () =>  {
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbols = () =>{
+    const symbols = "(){}[]=<>/,.!@#$%&*+-";
+    return symbols[Math.floor(Math.random() *symbols.length)];
+};
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase,getNumber, getSymbols) => {
+
+    let password = ""
+
+    const passwordLength = 10
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbols
+    ]
+
+    for(i = 0; i < passwordLength; i = i + generators.length) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+            password+= randomValue;
+        });
+    }
+
+    password = password.slice(0, passwordLength);
+    generatedPasswordElement.style.display = "block"
+    generatedPasswordElement.querySelector("h4").innerText = password;
+}
+
+// Eventos 
+generatePasswordButton.addEventListener("click", (e) => {
+    generatePassword(
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbols
+    );
+});
